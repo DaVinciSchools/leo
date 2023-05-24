ef0c8c27270bfcc087d44e6308994eff0f8cde2a
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-05-23 17:56:32 -0700

    Logout link uses a POST form submission.

--- server/src/main/clients/web-browser/src/libs/DefaultPage/DefaultPage.tsx
+++ server/src/main/clients/web-browser/src/libs/DefaultPage/DefaultPage.tsx
@@ -1,17 +1,19 @@
 import './DefaultPage.scss';
 
-import {getCurrentUser} from '../authentication';
+import {addXsrfInputField, getCurrentUser} from '../authentication';
 import {
   BellOutlined,
   QuestionCircleOutlined,
   SmileTwoTone,
 } from '@ant-design/icons';
-import {PropsWithChildren} from 'react';
+import {PropsWithChildren, useRef} from 'react';
 import {Popover} from 'antd';
 import {Link} from 'react-router-dom';
 
 export function DefaultPage(props: PropsWithChildren<{title: string}>) {
   const user = getCurrentUser(() => {});
+  const formRef = useRef<HTMLFormElement>(null);
+
   const avatarPanel = (
     <>
       <div className="avatar-panel">
@@ -29,7 +31,15 @@ export function DefaultPage(props: PropsWithChildren<{title: string}>) {
           <Link to="/users/my-account">My Account</Link>
         </div>
         <div className="menu">
-          <Link to="/users/logout">Logout</Link>
+          <form action="/api/logout" method="POST" ref={formRef}>
+            {addXsrfInputField()}
+            <span
+              onClick={() => formRef.current!.requestSubmit()}
+              style={{cursor: 'pointer'}}
+            >
+              Log out
+            </span>
+          </form>
         </div>
       </div>
     </>
--- server/src/main/clients/web-browser/src/libs/authentication.tsx
+++ server/src/main/clients/web-browser/src/libs/authentication.tsx
@@ -1,6 +1,7 @@
 import {pl_types} from '../generated/protobuf-js';
 import IUser = pl_types.IUser;
 import User = pl_types.User;
+import Cookies from 'js-cookie';
 
 export enum Role {
   ADMIN,
@@ -16,6 +17,30 @@ export type LoggedInUser = {
   emailAddress: string;
   roles: Set<Role>;
 };
+
+export function addXsrfHeader(headers?: HeadersInit) {
+  if (Cookies.get('XSRF-TOKEN') != null) {
+    return {
+      ...(headers != null ? headers : {}),
+      ...{'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN')!},
+    };
+  }
+  return headers != null ? headers : {};
+}
+
+export function addXsrfInputField() {
+  if (Cookies.get('XSRF-TOKEN') != null) {
+    return (
+      <input
+        id="_csrf"
+        name="_csrf"
+        type="hidden"
+        value={Cookies.get('XSRF-TOKEN')!}
+      />
+    );
+  }
+  return <></>;
+}
 
 export function login(user: IUser) {
   localStorage.setItem('user', JSON.stringify(user));
