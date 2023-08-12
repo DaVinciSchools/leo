fe8f67ce02fe91afcbfde5d30f99499180a97d0d
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-08-11 23:42:47 -0700

    Add utility to normalize MUI Grid breakpoints.

--- server/src/main/clients/web-browser/src/libs/tags.ts
+++ server/src/main/clients/web-browser/src/libs/tags.ts
@@ -1,3 +1,5 @@
+import {RegularBreakpoints} from '@mui/material/Grid/Grid';
+
 export function addClassName(
   tagProps: {className?: string},
   additionalClassNames: string
@@ -9,4 +11,20 @@ export function addClassName(
     (newTagProps.className ?? '')
   ).trim();
   return newTagProps;
+}
+
+export function spread(breakpoints: RegularBreakpoints) {
+  const newBreakpoints = {...breakpoints};
+  newBreakpoints.xs =
+    breakpoints.xs ??
+    breakpoints.sm ??
+    breakpoints.md ??
+    breakpoints.lg ??
+    breakpoints.xl;
+  newBreakpoints.sm =
+    breakpoints.sm ?? breakpoints.md ?? breakpoints.lg ?? breakpoints.xl;
+  newBreakpoints.md = breakpoints.md ?? breakpoints.lg ?? breakpoints.xl;
+  newBreakpoints.lg = breakpoints.lg ?? breakpoints.xl;
+  newBreakpoints.xl = breakpoints.xl;
+  return newBreakpoints;
 }
