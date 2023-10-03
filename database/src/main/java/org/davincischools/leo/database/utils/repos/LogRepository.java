0f745f5594d9d97105f7725916d61ff590bf0a74
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-10-02 19:04:35 -0700

    Reduce max log field size. MySQL seems to spend a long time saving log entries.

--- database/src/main/java/org/davincischools/leo/database/utils/repos/LogRepository.java
+++ database/src/main/java/org/davincischools/leo/database/utils/repos/LogRepository.java
@@ -8,25 +8,31 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LogRepository extends JpaRepository<Log, Integer> {
 
+  private static int convertByteCountToCharCount(long bytes) {
+    return (int)
+        Math.min(
+            Integer.MAX_VALUE,
+            Math.min(
+                Math.round(
+                    Math.floor(bytes / StandardCharsets.UTF_8.newEncoder().maxBytesPerChar())),
+                bytes / /* MySql bytes per char using UTF8MB4 */ 4));
+  }
+
   long LONGTEXT_MAX_BYTES = /* 4GB= */ 4L * 1024 * 1024 * 1024;
-  int LONGTEXT_MAX_CHARS =
-      (int)
-          Math.min(
-              Integer.MAX_VALUE,
-              Math.min(
-                  Math.round(
-                      LONGTEXT_MAX_BYTES / StandardCharsets.UTF_8.newEncoder().maxBytesPerChar()),
-                  LONGTEXT_MAX_BYTES / /* MySql bytes per char using UTF8MB4 */ 4));
+  int LONGTEXT_MAX_CHARS = convertByteCountToCharCount(LONGTEXT_MAX_BYTES);
+
+  int MAX_LOG_BYTES = /* 16MB= */ 16 * 1024 * 1024;
+  int MAX_LOG_CHARS = convertByteCountToCharCount(MAX_LOG_BYTES);
 
   enum Status {
     ERROR,
     SUCCESS
   }
 
-  public static String trimToLogLength(String input) {
-    if (input.length() < LONGTEXT_MAX_CHARS) {
+  static String trimToLogLength(String input) {
+    if (input.length() < MAX_LOG_CHARS) {
       return input;
     }
-    return input.substring(0, Math.min(input.length(), LONGTEXT_MAX_CHARS));
+    return input.substring(0, MAX_LOG_CHARS);
   }
 }
