4686b45255c03c0d40e21c9767e840ee933aaa67
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-10-04 03:19:49 -0700

    Simplify use of EditableReactQuill. Only create one element at a time.

--- server/src/main/clients/web-browser/src/libs/EditableReactQuill/EditableReactQuill.tsx
+++ server/src/main/clients/web-browser/src/libs/EditableReactQuill/EditableReactQuill.tsx
@@ -5,6 +5,7 @@ import {CSSProperties, ReactNode, useEffect, useRef} from 'react';
 export function EditableReactQuill(props: {
   value: string | undefined | null;
   onChange?: (value: string) => void;
+  onClick?: (event: MouseEvent) => void;
 
   editing?: boolean;
   editingStyle?: CSSProperties;
@@ -15,51 +16,90 @@ export function EditableReactQuill(props: {
   placeholder?: ReactNode;
   className?: string;
 }) {
-  const editingRef = useRef<ReactQuill>(null);
+  const quillRef = useRef<ReactQuill>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (props.editing) {
-      editingRef.current?.focus();
+      quillRef.current?.focus();
     }
   }, [props.editing]);
 
+  function handleClick(event: MouseEvent) {
+    props.onClick?.(event);
+  }
+
+  function handleFocusOut(event: FocusEvent) {
+    let targetReceivingFocus = event.relatedTarget;
+    while (
+      targetReceivingFocus != null &&
+      targetReceivingFocus instanceof HTMLElement
+    ) {
+      if (targetReceivingFocus === divRef.current) {
+        return;
+      }
+      targetReceivingFocus = targetReceivingFocus.parentElement;
+    }
+    props.onBlur?.();
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    // 'keypress' is not fired for the Escape key.
+    if (event.key === 'Escape') {
+      props.onBlur?.();
+    }
+  }
+
+  useEffect(() => {
+    if (divRef.current != null) {
+      const lastDivRef = divRef.current;
+      lastDivRef.addEventListener('click', handleClick);
+      lastDivRef.addEventListener('focusout', handleFocusOut);
+      lastDivRef.addEventListener('keydown', handleKeyDown);
+      return () => {
+        lastDivRef.removeEventListener('click', handleClick);
+        lastDivRef.removeEventListener('focusout', handleFocusOut);
+        lastDivRef.addEventListener('keydown', handleKeyDown);
+      };
+    }
+    return;
+  });
+
   return (
     <>
-      <div style={{display: props.editing ? undefined : 'none'}}>
-        <ReactQuill
-          ref={editingRef}
-          theme="snow"
-          className={'global-react-quill ' + props.className}
-          value={props.value ?? ''}
-          onChange={value => {
-            if (props.editing) {
+      <div ref={divRef}>
+        {props.editing ? (
+          <ReactQuill
+            key="Editing"
+            ref={quillRef}
+            theme="snow"
+            className={'global-react-quill ' + props.className}
+            value={props.value ?? ''}
+            preserveWhitespace={true}
+            onChange={value => {
               props.onChange?.(value);
+            }}
+            style={{...(props.editingStyle ?? {})}}
+          />
+        ) : isHtmlEmpty(props.value) ? (
+          props.placeholder ?? <></>
+        ) : (
+          <ReactQuill
+            key="ReadOnly"
+            ref={quillRef}
+            theme="snow"
+            className={
+              'global-react-quill global-react-quill-read-only ' +
+              props.className
             }
-          }}
-          onBlur={props.onBlur}
-          preserveWhitespace={true}
-          style={{
-            display: props.editing ? undefined : 'none',
-            ...props.editingStyle,
-          }}
-        />
+            value={props.value ?? ''}
+            preserveWhitespace={true}
+            modules={{toolbar: false}}
+            readOnly={true}
+            style={{...(props.readOnlyStyle ?? {})}}
+          />
+        )}
       </div>
-      <ReactQuill
-        theme="snow"
-        className={
-          'global-react-quill global-react-quill-read-only ' + props.className
-        }
-        value={props.value ?? ''}
-        preserveWhitespace={true}
-        modules={{toolbar: false}}
-        readOnly={true}
-        style={{
-          display:
-            !props.editing && !isHtmlEmpty(props.value) ? undefined : 'none',
-          ...props.readOnlyStyle,
-        }}
-      />
-      {!props.editing && isHtmlEmpty(props.value) && props.placeholder}
     </>
   );
 }
