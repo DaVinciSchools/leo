145eace1f8f337fb94a2750d1253531a5cb1248a
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-12-13 22:18:02 -0800

    Add missing @Repository annotation to ProjectPostRatingRepository.java

--- database/src/main/java/org/davincischools/leo/database/utils/repos/ProjectPostRatingRepository.java
+++ database/src/main/java/org/davincischools/leo/database/utils/repos/ProjectPostRatingRepository.java
@@ -2,7 +2,9 @@ package org.davincischools.leo.database.utils.repos;
 
 import org.davincischools.leo.database.daos.ProjectPostRating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectPostRatingRepository extends JpaRepository<ProjectPostRating, Integer> {
   enum RatingType {
     INITIAL_1_TO_5,
